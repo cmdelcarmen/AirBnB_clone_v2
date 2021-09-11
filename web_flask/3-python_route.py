15b870ece2d5755314aa95daf3ee6f81b9814416
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-11 20:25:07 +0000

    fixed print

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,17 +9,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_test(text):
     return "C {}".format(text.replace('_', ' '))
 
@@ -31,5 +31,4 @@ def python_text(text="is cool"):
 
 if __name__ == "__main__":
     app.debug = True
-    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port=5000)
