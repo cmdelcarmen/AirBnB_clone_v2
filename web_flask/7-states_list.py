6cfaadd48e01775816cfe17e5f17116d87013ee6
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-12 05:39:31 +0000

    fixed some things up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,14 +9,14 @@ from models import storage
 
 app = Flask(__name__)
 
-
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    all_states = storage.all('State')
-    return render_template('/7-states_list.html', states=all_states)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(ext):
     storage.close()
 
 if __name__ == "__main__":
