6fd17179bace0641ce62b55b12b4e84c0147cf85
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-12 19:57:59 +0000

    fixed up num 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,13 +6,14 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    states = storage.all("State")
+    states = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 
