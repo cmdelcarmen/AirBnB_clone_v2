0de3cd47cbb060b93164244f7cee57c636148399
Caroline Del Carmen <delcarmencaroline@gmail.com>
2021-09-12 20:59:08 +0000

    fixed if statement

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,10 +11,16 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
+def cities_by_state_list():
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
+
 
 
 @app.teardown_appcontext
